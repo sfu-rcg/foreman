6c32b900f15fcc2ba24ae9e445bc11c71ac9c395
Riley Shott <rshott@sfu.ca>
2014-10-16 14:16:54 -0700

    Added additional plugin
    Added the foreman_param_lookup plugin, which is needed for Fancyass

--- bundler.d/Gemfile.local.rb
+++ bundler.d/Gemfile.local.rb
@@ -1,3 +1,4 @@
-# Custom hooks used by SFU
+# Custom plugins used by SFU
+gem 'foreman_param_lookup', :git => "https://github.com/sfu-rcg/foreman_param_lookup.git"
 gem 'foreman_hooks', :git => 'https://github.com/theforeman/foreman_hooks.git'
 gem 'foreman_registration', :git => 'https://github.com/sfu-rcg/foreman_registration.git'
