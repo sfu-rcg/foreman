d99ce3836765c64bc5531930607504f8e2033903
Riley Shott <rshott@sfu.ca>
2014-10-16 11:56:23 -0700

    Add Gemfile.local.rb
    Added Gemfile.local.rb with the necessary plugins we'll be using

---
+++ bundler.d/Gemfile.local.rb
@@ -0,0 +1,3 @@
+# Custom hooks used by SFU
+gem 'foreman_hooks', :git => 'https://github.com/theforeman/foreman_hooks.git'
+gem 'foreman_registration', :git => 'https://github.com/sfu-rcg/foreman_registration.git'
