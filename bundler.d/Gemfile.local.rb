67d0acec4953ce327baf614797e8e16b8a3623dc
Riley Shott <rshott@fry.rcg.sfu.ca>
2014-11-24 12:04:26 -0800

    Added additional plugin
    Added the foreman_host_overview plugin as it provides handy VNC & RDP shortcuts,
    and displays a host's certificate name.

--- bundler.d/Gemfile.local.rb
+++ bundler.d/Gemfile.local.rb
@@ -2,3 +2,4 @@
 gem 'foreman_param_lookup', :git => "https://github.com/sfu-rcg/foreman_param_lookup.git"
 gem 'foreman_hooks', :git => 'https://github.com/theforeman/foreman_hooks.git'
 gem 'foreman_registration', :git => 'https://github.com/sfu-rcg/foreman_registration.git'
+gem 'foreman_host_overview', :git => 'https://github.com/sfu-rcg/foreman_host_overview.git'
